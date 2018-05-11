79ed4e5d901cdbfa41da9e6ebca91a3d11e1dcb2
Unknown <karoline.adnanes@gmail.com>
2018-05-11 13:04:26 +0200

    Kommentert ut Game over

--- src/game/view/Level.java
+++ src/game/view/Level.java
@@ -7,12 +7,17 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.effect.Blend;
+import javafx.scene.effect.BlendMode;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.InnerShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import game.controller.GameController;
 import game.model.Bear;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -64,15 +69,53 @@ public class Level extends Pane {
                     for (int i = 0; i < gameOver.toCharArray().length; i++) {
                         char letter = gameOver.charAt(i);
 
-                        Text text = new Text(String.valueOf(letter));
-                        text.setFont(Font.font(48));
-                        text.setFont(Font.font("Avenir Next", FontWeight.BOLD, 45));
+                        /*Text text = new Text(String.valueOf(letter));
+                        text.setFont(Font.font("Avenir Next", FontWeight.BOLD, 55));
+                        text.setFill(Color.YELLOW);
 
                         hBox.getChildren().add(text);
 
-                        FadeTransition ft = new FadeTransition();
-                        ft.setToValue(1);
-                        ft.play();
+
+                        Blend blend = new Blend();
+                        blend.setMode(BlendMode.MULTIPLY);
+
+                        DropShadow ds = new DropShadow();
+                        ds.setColor(Color.rgb(254, 235, 66, 0.3));
+                        ds.setOffsetX(5);
+                        ds.setOffsetY(5);
+                        ds.setRadius(5);
+                        ds.setSpread(0.2);
+
+                        blend.setBottomInput(ds);
+
+                        DropShadow ds1 = new DropShadow();
+                        ds1.setColor(Color.web("#f13a00"));
+                        ds1.setRadius(20);
+                        ds1.setSpread(0.2);
+
+                        Blend blend2 = new Blend();
+                        blend2.setMode(BlendMode.MULTIPLY);
+
+                        InnerShadow is = new InnerShadow();
+                        is.setColor(Color.web("#feeb42"));
+                        is.setRadius(9);
+                        is.setChoke(0.8);
+                        blend2.setBottomInput(is);
+
+                        InnerShadow is1 = new InnerShadow();
+                        is1.setColor(Color.web("#f13a00"));
+                        is1.setRadius(5);
+                        is1.setChoke(0.4);
+                        blend2.setTopInput(is1);
+
+                        Blend blend1 = new Blend();
+                        blend1.setMode(BlendMode.MULTIPLY);
+                        blend1.setBottomInput(ds1);
+                        blend1.setTopInput(blend2);
+
+                        blend.setTopInput(blend1);
+
+                        text.setEffect(blend);*/
 
                     }
 
