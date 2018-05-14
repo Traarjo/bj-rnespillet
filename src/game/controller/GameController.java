77efa3f27c29b327bfb0cbb9eb85999d8cc4b081
Tara Jørgensen <traarj@gmail.com>
2018-05-14 17:41:16 +0200

    endret oppsettet til isgameover

--- src/game/controller/GameController.java
+++ src/game/controller/GameController.java
@@ -297,7 +297,10 @@ public class GameController {
     }
 
     public boolean isGameOver(){
-         return bear == null ||  bear.getLives() <= 0;
+        if (bear == null ||  bear.getLives() <= 0) {
+            return true;
+        }
+        return false;
     }
 
     //dele opp denne?
